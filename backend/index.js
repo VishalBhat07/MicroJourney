3f3f89dae2156516aaf4d99b685d8ec5d12b6095
VishalBhat07 <vishalbhat21092005@gmail.com>
2025-08-31 10:24:34 +0530

    feeat: Initialise backend with mongo setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Server healthy!");
+});
+
+app.listen(port, () => {
+  console.log(`Backend running on port ${port}`);
+});
